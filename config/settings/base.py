b93d87db795f4a2baab2f647147e08cbb735c940
YuYa-MoriMoto <gyu2243dev@gmail.com>
2022-04-03 23:10:08 +0900

    [add]startapp account

--- config/settings/base.py
+++ config/settings/base.py
@@ -6,6 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 INSTALLED_APPS = [
     'channels',
     'chat',
+    'account',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
