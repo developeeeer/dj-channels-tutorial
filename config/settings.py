c172a40af2de94d0db8d5a7801ed0705857396a3
YuYa-MoriMoto <gyu2243dev@gmail.com>
2022-04-03 15:39:52 +0900

    [add]chat applicationのセットアップ

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(',')
 # Application definition
 
 INSTALLED_APPS = [
+    'chat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
